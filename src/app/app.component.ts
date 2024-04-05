eebd03f0c282888b5182c191c64a9431ab7397ab
kacper.sledz <casper.sledx@gmail.com>
2024-04-05 19:57:20 +0200

    basic header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatIconModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'strona';
+  menuOpen = false;
+
+  menuOptions = [
+    { name: 'Ogłoszenia', path: '/ogloszenia' },
+    { name: 'Intencje', path: '/intencje' },
+    { name: 'Nabożeństwa', path: '/nabozenstwa' },
+    { name: 'Galeria', path: '/galeria' },
+  ];
+
+  toggle() {
+    this.menuOpen = !this.menuOpen;
+  }
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
